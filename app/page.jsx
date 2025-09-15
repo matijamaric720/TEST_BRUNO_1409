2480504447ffd59958f1753b792e85c4fde70850
matijamaric720 <matijamaric720@gmail.com>
2025-09-15 23:35:21 +0200

    Update pagejsx

--- app/page.jsx
+++ app/page.jsx
@@ -13,25 +13,21 @@ import Services from "@/components/homes/home-10/Services";
 import Testimonials from "@/components/common/testimonials/Testimonials";
 import MobailHeader1 from "@/components/headers/MobailHeader1";
 import Script from "next/script";
+
 export const metadata = {
-  title:
-    "Home 1",
-  description:
-    "BONE Travel agencija",
+  title: "Home 1",
+  description: "BONE Travel agencija",
 };
 
-const GOOGLE_MAPS_API_KEY = "AIzaSyCP5CQEaW-KFtwaCMSt8oO_uYTIYP7kvOs";
-
-
-
-export default function page() {
+export default function Page() {
   return (
     <>
-    <Script
-        strategy="beforeInteractive"
-        src={`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=places`}
+      <Script
+        strategy="afterInteractive"
+        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
       />
-      <Header9 /> <MobailHeader1 />
+      <Header9 />
+      <MobailHeader1 />
       <main className="main">
         <Hero />
         <Features />
